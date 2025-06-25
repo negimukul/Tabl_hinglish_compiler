a51207671635305ac46d458fdd70302ec36cdc6e
negimukul <negim8205@gmail.com>
2025-06-25 20:53:02 +0530

    Delete test_hinglish_sql_2.py

--- test_hinglish_sql_2.py
+++
@@ -1,6 +0,0 @@
-from compiler import execute
-
-print(execute("bana table students"))
-print(execute("daal mein students value 'Shobhit'"))
-print(execute("daal mein students value 'Alice'"))
-print(execute("nikal se students"))
