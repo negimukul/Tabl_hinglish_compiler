31310ab018138ee0bbfe79b23193655e62ac3c7c
negimukul <negim8205@gmail.com>
2025-06-25 20:53:50 +0530

    Delete compiler directory

--- compiler/__init__.py
+++
@@ -1,8 +0,0 @@
-# __init__.py
-
-from .lexer import tokenize
-from .parser import parse
-from .semantic import check_ast
-from .interpreter import execute
-
-__all__ = ["tokenize", "parse", "check_ast", "execute"]
--- compiler/test.py
+++
@@ -1,7 +0,0 @@
-from lexer import tokenize
-from parser import parse
-
-code = "bana table students"
-tokens = tokenize(code)
-ast = parse(tokens)
-print(ast)
